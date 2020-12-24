125d8d2be32ff2a140654cf129593877eb63ec48
You <youux@outlook.com>
2020-12-25 00:13:07 +0800

    init

--- .nuxt/router.js
+++ .nuxt/router.js
@@ -3,13 +3,13 @@ import Router from 'vue-router'
 import { interopDefault } from './utils'
 import scrollBehavior from './router.scrollBehavior.js'
 
-const _66b4aec8 = () => interopDefault(import('..\\pages\\layout' /* webpackChunkName: "" */))
-const _af67525e = () => interopDefault(import('..\\pages\\home' /* webpackChunkName: "" */))
-const _6176cbb7 = () => interopDefault(import('..\\pages\\login' /* webpackChunkName: "" */))
-const _8fcceb12 = () => interopDefault(import('..\\pages\\profile' /* webpackChunkName: "" */))
-const _475055d6 = () => interopDefault(import('..\\pages\\settings' /* webpackChunkName: "" */))
-const _7e526242 = () => interopDefault(import('..\\pages\\editor' /* webpackChunkName: "" */))
-const _c2386478 = () => interopDefault(import('..\\pages\\article' /* webpackChunkName: "" */))
+const _70c2097a = () => interopDefault(import('..\\pages\\layout' /* webpackChunkName: "" */))
+const _d1b151a2 = () => interopDefault(import('..\\pages\\home' /* webpackChunkName: "" */))
+const _4dfbd719 = () => interopDefault(import('..\\pages\\login' /* webpackChunkName: "" */))
+const _d0ed754e = () => interopDefault(import('..\\pages\\profile' /* webpackChunkName: "" */))
+const _2a41131a = () => interopDefault(import('..\\pages\\settings' /* webpackChunkName: "" */))
+const _64f32fbd = () => interopDefault(import('..\\pages\\editor' /* webpackChunkName: "" */))
+const _7e5388a6 = () => interopDefault(import('..\\pages\\article' /* webpackChunkName: "" */))
 
 // TODO: remove in Nuxt 3
 const emptyFn = () => {}
@@ -29,34 +29,34 @@ export const routerOptions = {
 
   routes: [{
     path: "/",
-    component: _66b4aec8,
+    component: _70c2097a,
     children: [{
       path: "",
-      component: _af67525e,
+      component: _d1b151a2,
       name: "home"
     }, {
       path: "/login",
-      component: _6176cbb7,
+      component: _4dfbd719,
       name: "login"
     }, {
       path: "/register",
-      component: _6176cbb7,
+      component: _4dfbd719,
       name: "register"
     }, {
       path: "/profile/:username",
-      component: _8fcceb12,
+      component: _d0ed754e,
       name: "profile"
     }, {
       path: "/settings",
-      component: _475055d6,
+      component: _2a41131a,
       name: "settings"
     }, {
       path: "/editor/:slug?",
-      component: _7e526242,
+      component: _64f32fbd,
       name: "editor"
     }, {
       path: "/article/:slug",
-      component: _c2386478,
+      component: _7e5388a6,
       name: "article"
     }]
   }],
--- README.md
+++ README.md
@@ -2,3 +2,18 @@
 
 # 部署完成：
 http://111.229.92.196:3000/
+
+列出tag
+```
+git tag
+```
+
+新建tag
+```
+git tag tagName
+```
+
+推送tag到远程
+```
+git push origin tagName
+```
